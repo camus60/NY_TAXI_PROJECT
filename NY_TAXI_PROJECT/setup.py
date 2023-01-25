cecd589b3aed8e62ffd4d4157323bfde72f5dabb
camus60 <mkambona@gmail.com>
2023-01-25 11:19:57 +0300

    initial commit

---
+++ NY_TAXI_PROJECT/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='peline project ',
+    author='camus60',
+    license='',
+)
